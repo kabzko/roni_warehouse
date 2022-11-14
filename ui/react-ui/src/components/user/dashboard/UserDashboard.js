68bd215a45406d00607d2ffce473a1b65634b7c4
Reuel Dave Cardines <rdcardines@gmail.com>
2022-11-14 20:42:47 +0800

    updates

--- ui/react-ui/src/components/user/dashboard/UserDashboard.js
+++ ui/react-ui/src/components/user/dashboard/UserDashboard.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import * as onScan from 'onscan.js';
+
 class UserDashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             ...props,
         };
+    }
+
+    componentDidMount() {
+        onScan.attachTo(document);
+        document.addEventListener("scan", function(sScancode, iQuantity) {
+            console.log(sScancode.detail.scanCode, sScancode.detail.qty); 
+        });
+    }
+
+    componentWillUnmount() {
+        onScan.detachFrom(document);
+        document.removeEventListener("scan");
     }
 
     render() {
